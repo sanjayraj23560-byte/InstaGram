9a2532248275e566351fb71f9c71d93e97e0095a
Sanjay Raj <sanjayraj23560@gmail.com>
2026-08-27 17:01:19 +0530

    New code

---
+++ client/app/search/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+   <div>
+     <div className='border-amber-200 border-2 rounded-2xl mt-10  flex justify-center items-center' ><input type="text" /></div>
+
+   </div>
+  )
+}
+
+export default page
